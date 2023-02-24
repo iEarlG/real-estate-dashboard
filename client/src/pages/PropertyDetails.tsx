e9188184f5f4c087cf534af449841ada9dcfb896
Earl Villapaz <jasonburning16@gmail.com>
2023-02-24 14:44:23 +0800

    setting up the PropertyDetails on client side

--- client/src/pages/PropertyDetails.tsx
+++ client/src/pages/PropertyDetails.tsx
@@ -1,6 +1,19 @@
-import React from 'react'
+
+import { Typography, Box, Stack } from '@pankod/refine-mui';
+import { useDelete, useGetIdentity, useShow } from '@pankod/refine-core';
+import { useParams, useNavigate } from '@pankod/refine-react-router-v6';
+
+import { ChatBubble, Delete, Edit, Phone, Place, Star } from '@mui/icons-material';
+import { CustomBtn } from 'components';
 
 const PropertyDetails = () => {
+  const navigate = useNavigate();
+  const { data: user } = useGetIdentity();
+  const { id } = useParams();
+  const { mutate } = useDelete();
+  const { queryResult } = useShow();
+  const { data, isLoading, isError } = queryResult;
+
   return (
     <div>ProperyDetails</div>
   )
